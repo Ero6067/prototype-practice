eb28145224e6416206a92cf8b56e124ddcc7c6a8
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-06-11 00:38:17 -0700

    adding prototypical inheritance example

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function Person(firstName, lastName) {
 }
 
 //Greeting
-Person.prototype.greeting = function(){
+Person.prototype.greeting = function() {
   return `Hello there ${this.firstName} ${this.lastName}`;
 }
 
@@ -15,11 +15,25 @@ const person1 = new Person( 'John', 'Doe' )
 //Customer Constructor
 function Customer(firstName, lastName, phone, membership) {
   Person.call(this, firstName, lastName);
+
   this.phone = phone;
   this.membership = membership;
 }
+
+//Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+//Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
 
 //Create customer
 const customer1 = new Customer('Tom', 'King', '123-456-7890', 'Standard');
 
 console.log(customer1);
+
+//Customer greeting
+Customer.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
+}
+
+console.log(customer1.greeting());
